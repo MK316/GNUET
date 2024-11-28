e07bb793082e3905bfe6f6b433ad032f3249601d
MK <99416359+MK316@users.noreply.github.com>
2024-11-28 15:33:17 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
-st.title("Gallery for Nurturing Upcoming English Teachers")
+title_html = """
+<h1 style='text-align: center;'>
+    <span style='color: gray;'>G</span>
+    <span style='color: gray;'>allery for </span>
+    <span style='color: gray;'>N</span>
+    <span style='color: gray;'>urturing </span>
+    <span style='color: gray;'>U</span>
+    <span style='color: gray;'>pcoming English Teachers</span>
+</h1>
+"""
+
+st.markdown(title_html, unsafe_allow_html=True)
+
 st.caption("Welcome to the Gallery for Nurturing Upcoming English Teachers (GNU-ET), where I've carefully selected a range of tools designed and developed specifically to support your journey toward the English teacher qualification exam. Each application in this gallery is crafted to enhance your personal study experience. I hope these resources prove invaluable as you progress towards your certification goals.")
 st.write("In preparation (since Nov.28, 2024)")
