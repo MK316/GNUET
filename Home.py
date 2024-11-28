2d8ae0c92f02a81a7c08209012c43046f0b9d413
MK <99416359+MK316@users.noreply.github.com>
2024-11-28 15:30:27 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.title("Gallery for Nurturing Upcoming English Teachers")
-st.caption("Welcome to the Gallery for Nurturing Upcoming English Teachers (GNUE), where I've carefully selected a range of tools designed and developed specifically to support your journey toward the English teacher qualification exam. Each application in this gallery is crafted to enhance your personal study experience. I hope these resources prove invaluable as you progress towards your certification goals.")
+st.caption("Welcome to the Gallery for Nurturing Upcoming English Teachers (GNU-ET), where I've carefully selected a range of tools designed and developed specifically to support your journey toward the English teacher qualification exam. Each application in this gallery is crafted to enhance your personal study experience. I hope these resources prove invaluable as you progress towards your certification goals.")
 st.write("In preparation (since Nov.28, 2024)")
